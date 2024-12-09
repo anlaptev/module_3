ba7c97b7c0f978a7a21abe6afd39a97c73976743
anlaptev <anlaptev@gmail.com>
2024-12-09 20:21:12 +0300

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -2,16 +2,13 @@ def single_root_words(root_word, *other_words):
     same_words = []
     root_word = root_word.lower()
     for i in other_words:
-        if len(root_word) <= len(i):
-            if root_word in i.lower():
-                same_words.append(i)
-        else:
-            if i.lower() in root_word:
+        word_from_list = i.lower()
+        if root_word in word_from_list or word_from_list in root_word:
                 same_words.append(i)
     return same_words
 
 
-result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies', 'Ric')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result1)
 print(result2)
